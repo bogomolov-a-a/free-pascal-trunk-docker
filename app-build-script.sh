3a59185cef120edf9ddb6d1ef6fc37adbb4cd7eb
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2022-02-25 18:01:38 +0300

    remove wrong shell script

--- app-build-script.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/bogomolov-a-a/free-pascal-application-util-packages.git&&\
-$LAZ_BUILD_PATH --verbose --lazarusdir=$LAZ_SHARE_DIR --compiler=$COMPILER_PATH ./free-pascal-application-util-packages/freepascal-application-common/src/main/freepascalapplicationcommon.lpk&&\
-ls ./$LAZ_BUILD_DSL_APP_SOURCE_PATH -a &&\
-$LAZ_BUILD_PATH --verbose --lazarusdir=$LAZ_SHARE_DIR --compiler=$COMPILER_PATH ./$LAZ_BUILD_DSL_APP_SOURCE_PATH/$LAZ_BUILD_DSL_APP_NAME.lpi&&\
-cp ./target/$LAZ_BUILD_DSL_APP_NAME ./$LAZ_BUILD_DSL_APP_NAME&&\
-rm ./$LAZ_BUILD_DSL_APP_SOURCE_PATH -rf&&
-rm ./target -rf&&
-rm ./free-pascal-application-util-packages -rf
--- lazbuild-dsl-app/app-build-script.sh
+++ lazbuild-dsl-app/app-build-script.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/bogomolov-a-a/free-pascal-application-util-packages.git&&\
-$LAZ_BUILD_PATH --verbose --lazarusdir=$LAZDIR --compiler=$COMPILER_PATH ./free-pascal-application-util-packages/freepascal-application-common/src/main/freepascalapplicationcommon.lpk&&\
+$LAZ_BUILD_PATH --verbose --lazarusdir=$LAZ_DIR --compiler=$COMPILER_PATH ./free-pascal-application-util-packages/freepascal-application-common/src/main/freepascalapplicationcommon.lpk&&\
 ls ./$LAZ_BUILD_DSL_APP_SOURCE_PATH -a &&\
-$LAZ_BUILD_PATH --verbose --lazarusdir=$LAZDIR --compiler=$COMPILER_PATH ./$LAZ_BUILD_DSL_APP_SOURCE_PATH/$LAZ_BUILD_DSL_APP_NAME.lpi&&\
+$LAZ_BUILD_PATH --verbose --lazarusdir=$LAZ_DIR --compiler=$COMPILER_PATH ./$LAZ_BUILD_DSL_APP_SOURCE_PATH/$LAZ_BUILD_DSL_APP_NAME.lpi&&\
 cp ./target/$LAZ_BUILD_DSL_APP_NAME ./$LAZ_BUILD_DSL_APP_NAME&&\
 rm ./$LAZ_BUILD_DSL_APP_SOURCE_PATH -rf&&
 rm ./target -rf&&
